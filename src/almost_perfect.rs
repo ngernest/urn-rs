9cab076114afff97ea5f185a28ec4d163672667f
Ernest Ng <eyn5@cornell.edu>
2024-12-26 13:07:03 -0600

    Unit tests for reverse_bits

--- src/almost_perfect.rs
+++ src/almost_perfect.rs
@@ -27,8 +27,24 @@ mod tests {
 
     #[test]
     fn test_reverse_bits() {
-        let expected = 0b011;
-        let actual = reverse_bits(3, 0b110);
-        assert_eq!(expected, actual)
+        // Case `|n| < |x|` : n=3, x=14 (0b1110) -> 0b111
+        // Leftmost 1 in `0b1110` is ignored
+        assert_eq!(reverse_bits(3, 0b1110), 0b011);
+
+        // Case `|n| = |x|` : n=4, x=12 (0b1100) -> 0b0011
+        // Reverses the entire binary representation of `x`
+        assert_eq!(reverse_bits(4, 0b1100), 0b0011);
+
+        // Case `|n| > |x|` : n=5, x=3 (0b11) -> 0b11000
+        // `reverse_bits` adds trailing zeroes in this case
+        assert_eq!(reverse_bits(5, 0b11), 0b11000);
+
+        // Case `x = 0` : n=3, x=0 (0b000) -> 0b000
+        // Ensures that 000 is handled correctly
+        assert_eq!(reverse_bits(3, 0b000), 0b000);
+
+        // Case `|n| = |x| = 1`: n=1, x=1 (0b1) -> 0b1
+        // Reversing one bit does nothing
+        assert_eq!(reverse_bits(1, 0b1), 0b1);
     }
 }
