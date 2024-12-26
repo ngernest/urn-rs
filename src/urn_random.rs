808fbabd44d086f3dfd586c4aa4c001306cfc221
Ernest Ng <eyn5@cornell.edu>
2024-12-26 14:33:49 -0600

    Switch to using rand::gen_range for sample_weight

--- src/urn_random.rs
+++ src/urn_random.rs
@@ -2,13 +2,12 @@
 
 use crate::types::{Index, Tree, Tree::*, Urn, Weight};
 use crate::urn_deterministic::*;
-use rand::distributions::Uniform;
 use rand::prelude::*;
 
 /// Produces a value uniformly at random from the range `[0, w]`
 fn sample_weight(w: Weight) -> Weight {
     let mut rng = thread_rng();
-    rng.sample(Uniform::new_inclusive(0, w))
+    rng.gen_range(0..=w)
 }
 
 fn sampler<F, T>(f: F, urn: Urn<T>) -> T
