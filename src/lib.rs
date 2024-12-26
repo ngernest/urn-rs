c9f0509e97c9117b755052ec9682fa74fb8f32e1
Ernest Ng <eyn5@cornell.edu>
2024-12-25 21:20:30 -0600

    Update function

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,18 @@ type Weight = u32;
 type Index = Weight;
 
 /// Polymorphic binary trees, with a weight at each node/leaf
-#[derive(Debug, PartialEq)]
-enum Tree<T> {
+#[derive(Debug, PartialEq, Clone)]
+enum Tree<T: Clone> {
     Leaf(Weight, T),
     Node(Weight, Box<Tree<T>>, Box<Tree<T>>),
 }
 
-impl<T> Tree<T> {
+impl<T: Clone> Tree<T> {
     /// Retrieves the weight of a tree
-    fn weight(&self) -> &Weight {
+    fn weight(&self) -> Weight {
         match self {
-            Tree::Leaf(w, _) => w,
-            Tree::Node(w, _, _) => w,
+            Tree::Leaf(w, _) => *w,
+            Tree::Node(w, _, _) => *w,
         }
     }
 
@@ -28,7 +28,7 @@ impl<T> Tree<T> {
             Tree::Leaf(_, a) => a,
             Tree::Node(_, l, r) => {
                 let wl = l.weight();
-                if i < *wl {
+                if i < wl {
                     l.sample(i)
                 } else {
                     r.sample(i - wl)
@@ -37,8 +37,44 @@ impl<T> Tree<T> {
         }
     }
 
-    fn update<F>(&self, _upd: F, _i: Index) -> ((Weight, T), (Weight, T), Self) {
-        todo!()
+    /// Updates the element at index `i` using the functino `upd`, returning
+    /// the old and new (weight, element) pairs, as wel as the updated tree
+    fn update<F>(&self, upd: F, i: Index) -> ((Weight, &T), (Weight, &T), Self)
+    where
+        F: for<'a> Fn(Weight, &'a T) -> (Weight, &'a T),
+    {
+        match self {
+            Tree::Leaf(w, a) => {
+                let (w_new, a_new) = upd(*w, a);
+                ((*w, a), (w_new, a_new), Tree::Leaf(w_new, a_new.clone()))
+            }
+            Tree::Node(w, l, r) => {
+                let wl = l.weight();
+                if i < wl {
+                    let (old, new, l_new) = l.update(upd, i);
+                    (
+                        old,
+                        new,
+                        Tree::Node(
+                            w - old.0 + new.0,
+                            Box::new(l_new),
+                            r.clone(),
+                        ),
+                    )
+                } else {
+                    let (old, new, r_new) = r.update(upd, i - wl);
+                    (
+                        old,
+                        new,
+                        Tree::Node(
+                            w - old.0 + new.0,
+                            l.clone(),
+                            Box::new(r_new),
+                        ),
+                    )
+                }
+            }
+        }
     }
 }
 
